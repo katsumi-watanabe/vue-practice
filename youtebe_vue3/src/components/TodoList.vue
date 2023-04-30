e0c9f23077c4806f549b8c9f3687ac546da08f5e
渡邊克己 <ktm981124@gmail.com>
2023-04-30 14:09:35 +0900

    親コンポーネントemit使い方

---
+++ youtebe_vue3/src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['todos'])
+const emit = defineEmits(['removeTodo'])
+// 親コンポーネントからemitされたremoveTodoを受け取る
+const clickRemove = (i) => emit('removeTodo', i)
+</script>
+
+<template>
+<ul v-if="todos.length > 0">
+  <li v-for="(todo, i) in todos" v-bind:key="i">{{ todo }}
+    <span @click="clickRemove(i)" style="cursor: pointer;">×</span>
+  </li>
+</ul>
+<p v-else>※ Todoを追加してください</p>
+</template>
