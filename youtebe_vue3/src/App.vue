ecfd5ab10bf8a702134da75325900b08f19124da
渡邊克己 <ktm981124@gmail.com>
2023-04-30 13:54:28 +0900

    component

--- youtebe_vue3/src/App.vue
+++ youtebe_vue3/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, computed } from 'vue'
+import AppHeader from './components/AppHeader.vue'
+import { ref } from 'vue'
 
 const todos = ref([])
 const newTodo = ref('')
@@ -8,14 +9,21 @@ const addTodo = () => {
   todos.value.push(newTodo.value)
   newTodo.value = ''
 }
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1)
+}
 </script>
 
-
 <template>
-  <input type="text" size="30" v-model="newTodo">
-  <button @click="addTodo()">追加</button>
+<AppHeader color="red">My Todo</AppHeader>
+<input type="text" size="30" v-model="newTodo">
+<button @click="addTodo()">追加</button>
 
-  <ul>
-    <li v-for="(todo, i) in todos" v-bind:key="i">{{ i }}. {{ todo }}</li>
-  </ul>
+<ul v-if="todos.length > 0">
+  <li v-for="(todo, i) in todos" v-bind:key="i">{{ todo }}
+    <span @click="removeTodo(i)" style="cursor: pointer;">×</span>
+  </li>
+</ul>
+<p v-else>※ Todoを追加してください</p>
 </template>
