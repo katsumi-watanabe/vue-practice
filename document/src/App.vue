63a4467f5fdc0c3646a841eed06fc85c172c10a8
渡邊克己 <ktm981124@gmail.com>
2023-05-20 20:38:35 +0900

    動作確認

--- document/src/App.vue
+++ document/src/App.vue
@@ -1,47 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase';
+
+const introductions = ref([]);
+
+const getUsers = async () => {
+  let { data: users, error, status } = await supabase.from('users').select('*');
+
+  introductions.value = users;
+};
+
+getUsers();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Vue3でsupabase</h1>
+    <ul>
+      <li v-for="introduction in introductions" :key="introduction.id">{{ introduction.email }}</li>
+    </ul>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
