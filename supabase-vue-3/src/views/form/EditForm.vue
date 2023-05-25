b678256d50e98950e12ae47925aa879d9b9906a8
渡邊克己 <ktm981124@gmail.com>
2023-05-25 22:28:44 +0900

    wip:編集画面

--- supabase-vue-3/src/views/form/EditForm.vue
+++ supabase-vue-3/src/views/form/EditForm.vue
@@ -1,12 +1,70 @@
 <template>
-  <Form />
+  <Form :initialData="initialData" @updateData="updateData" />
 </template>
+
 <script>
-  import Form from '@/components/Form.vue'
-  export default {
-    name: 'EditForm',
-    components: {
-      Form
+import Form from '@/components/Form.vue'
+import { supabase } from '@/supabase';
+
+export default {
+  name: 'EditForm',
+  data() {
+    return {
+      initialData: null,
+      name: '',
+      email: '',
+      message: ''
+    };
+  },
+  created() {
+    const exampleId = 'exampleId';
+
+    // データの取得
+    supabase
+      .from('your_table_name')
+      .select()
+      .eq('id', exampleId)
+      .single()
+      .then(({ data, error }) => {
+        if (error) {
+          console.log(error);
+        } else {
+          this.initialData = data;
+          this.name = data.name;
+          this.email = data.email;
+          this.message = data.message;
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  methods: {
+    updateData() {
+      const exampleId = 'exampleId';
+
+      const updatedData = {
+        name: this.name,
+        email: this.email,
+        message: this.message
+      };
+
+      // データの更新
+      supabase
+        .from('your_table_name')
+        .update(updatedData)
+        .eq('id', exampleId)
+        .then(({ data, error }) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('データが更新されました');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
+};
 </script>
